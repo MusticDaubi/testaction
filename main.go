61b7803ae2937af60f5b09d5c9aacb2a4a8e3088
MusticDaubi <130508063+MusticDaubi@users.noreply.github.com>
2025-06-10 21:29:59 +0300

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 func main() {
   //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
   // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
   fmt.Println("Hello and welcome")
   
 }
